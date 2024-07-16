8d966fe0fe770033cb7a3578d44deda53835c6f9
yu <support@7trees.cn>
2024-07-16 21:53:55 +0800

    update V1.1.273

--- wsgi.py
+++ wsgi.py
@@ -20,8 +20,8 @@ def main():
         from src.database import test_database_connection, CheckDatabase
         test_database_connection()
         CheckDatabase()
-        print("从浏览器打开: http://127.0.0.1:5000")
-        serve(app, host='0.0.0.0', port=5000)
+        print("从浏览器打开: http://127.0.0.1:9421")
+        serve(app, host='0.0.0.0', port=9421)
 
 
 if __name__ == '__main__':
