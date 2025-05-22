b25386b49f32e8a8cc17a3732cbf702e97fbecdf
athenavi <support@7trees.cn>
2025-05-22 15:51:59 +0800

    fix README.md

--- src/error.py
+++
@@ -1,5 +0,0 @@
-from flask import render_template
-
-
-def error(message, status_code):
-    return render_template('inform.html', error=message, status_code=status_code), status_code
--- src/other/rand.py
+++
@@ -1,8 +0,0 @@
-import random
-
-
-def generate_random_text(length=4):
-    # 生成随机的验证码文本
-    characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
-    captcha_text = ''.join(random.choice(characters) for _ in range(length))
-    return captcha_text
