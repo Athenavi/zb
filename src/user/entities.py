36f22d91c9883391a96b3c5533147a0c46e490dd
athenavi <support@7trees.cn>
2025-09-16 20:03:17 +0800

    refactor: 使用SQLAlchemy替换原始数据库连接方式 refactor: 移除不必要的函数和优化现有函数

--- src/user/entities.py
+++ src/user/entities.py
@@ -1,168 +1,129 @@
-from src.database import get_db_connection
+from sqlalchemy.exc import SQLAlchemyError
+
+from src.database import SessionLocal
+from src.models import Article, User
 
 
 def auth_by_uid(article_id, user_id):
     try:
-        with get_db_connection() as db:
-            with db.cursor() as cursor:
-                query = "SELECT 1 FROM articles WHERE article_id = %s AND Status != 'Deleted' AND user_id = %s"
-                cursor.execute(query, (article_id, user_id))
-                return cursor.fetchone() is not None
-    except Exception as e:
+        session = SessionLocal()
+        article = session.query(Article).filter_by(article_id=article_id, user_id=user_id, Status='Deleted').first()
+        return article is not None
+    except SQLAlchemyError as e:
         print(f"An error occurred: {e}")
         return False
-
-
-def authorize_by_aid_deleted(article_id, user_id):
-    try:
-        with get_db_connection() as db:
-            with db.cursor() as cursor:
-                query = "SELECT 1 FROM articles WHERE article_id = %s AND Status = 'Deleted' AND user_id = %s"
-                cursor.execute(query, (article_id, user_id))
-                return cursor.fetchone() is not None
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        return False
-
-
-def get_user_id(user_name):
-    try:
-        with get_db_connection() as db:
-            with db.cursor() as cursor:
-                query = "SELECT id FROM users WHERE username = %s;"
-                cursor.execute(query, (user_name,))
-                result = cursor.fetchone()
-                if result:
-                    return result[0]
-    except Exception as e:
-        print(f"An error occurred: {e}")
-
-    return 0
-
-
-def get_user_sub_info(query, user_id):
-    db = None
-    user_sub_info = []
-    try:
-        db = get_db_connection()
-        with db.cursor() as cursor:
-            cursor.execute(query, (int(user_id),))
-            user_sub = cursor.fetchall()
-            subscribe_ids = [sub[0] for sub in user_sub]
-            if subscribe_ids:
-                placeholders = ', '.join(['%s'] * len(subscribe_ids))
-                query = f"SELECT id, username FROM users WHERE id IN ({placeholders});"
-                cursor.execute(query, tuple(subscribe_ids))
-                user_sub_info = cursor.fetchall()
-    except Exception as e:
-        print(f"An error occurred: {e}")
     finally:
-        if db is not None:
-            db.close()
-    return user_sub_info
+        session.close()
 
 
 def check_user_conflict(zone, value):
-    if zone == 'username':
-        query = "SELECT username FROM users"
-    elif zone == 'email':
-        query = "SELECT email FROM users"
-    else:
-        return False
     try:
-        with get_db_connection() as db:
-            with db.cursor() as cursor:
-                cursor.execute(query)
-                if value not in [row[0] for row in cursor.fetchall()]:
-                    return False
-        return False
-    except Exception as e:
+        session = SessionLocal()
+        if zone == 'username':
+            user = session.query(User).filter_by(username=value).first()
+        elif zone == 'email':
+            user = session.query(User).filter_by(email=value).first()
+        else:
+            return False
+        return user is not None
+    except SQLAlchemyError as e:
         print(f"Error getting user list: {e}")
         return False
+    finally:
+        session.close()
 
 
 def db_save_avatar(user_id, avatar_uuid):
-    db = None
+    session = None
     try:
-        db = get_db_connection()
-        with db.cursor() as cursor:
-            query = "UPDATE users SET profile_picture = %s WHERE id = %s"
-            cursor.execute(query, (avatar_uuid, user_id))
-            db.commit()
-    except Exception as e:
+        session = SessionLocal()
+        user = session.query(User).filter_by(id=user_id).first()
+        if user:
+            user.profile_picture = avatar_uuid
+            session.commit()
+    except SQLAlchemyError as e:
         print(f"Error saving avatar: {e} by user {user_id} avatar uuid: {avatar_uuid}")
+        if session:
+            session.rollback()
     finally:
-        if db is not None:
-            db.close()
+        if session:
+            session.close()
 
 
 def db_save_bio(user_id, bio):
-    db = None
+    session = None
     try:
-        db = get_db_connection()
-        with db.cursor() as cursor:
-            query = "UPDATE users SET bio = %s WHERE id = %s"
-            cursor.execute(query, (bio, user_id))
-            db.commit()
-    except Exception as e:
+        session = SessionLocal()
+        user = session.query(User).filter_by(id=user_id).first()
+        if user:
+            user.bio = bio
+            session.commit()
+    except SQLAlchemyError as e:
         print(f"Error saving bio: {e} by user {user_id} bio: {bio}")
+        if session:
+            session.rollback()
     finally:
-        if db is not None:
-            db.close()
+        if session:
+            session.close()
 
 
 def change_username(user_id, new_username):
     # 修改用户名将可能导致资料被意外删除,建议先导出您的资料再进行下一步操作
     # 导出资料: 点击右上角头像 -> 点击设置 -> 点击导出资料
-    db = None
+    session = None
     try:
-        db = get_db_connection()
-        with db.cursor() as cursor:
-            query = "UPDATE users SET username = %s WHERE id = %s"
-            cursor.execute(query, (new_username, user_id))
-            db.commit()
-    except Exception as e:
+        session = SessionLocal()
+        user = session.query(User).filter_by(id=user_id).first()
+        if user:
+            user.username = new_username
+            session.commit()
+    except SQLAlchemyError as e:
         print(f"Error changing username: {e} by user {user_id} new username: {new_username}")
+        if session:
+            session.rollback()
     finally:
-        if db is not None:
-            db.close()
+        if session:
+            session.close()
 
 
 def bind_email(user_id, param):
-    check_user_conflict('email', param)
-    db = None
+    if check_user_conflict('email', param):
+        return False
+    session = None
     try:
-        db = get_db_connection()
-        with db.cursor() as cursor:
-            query = "UPDATE users SET email = %s WHERE id = %s"
-            cursor.execute(query, (param, user_id))
-            db.commit()
-    except Exception as e:
+        session = SessionLocal()
+        user = session.query(User).filter_by(id=user_id).first()
+        if user:
+            user.email = param
+            session.commit()
+    except SQLAlchemyError as e:
         print(f"Error binding email: {e} by user {user_id} email: {param}")
+        if session:
+            session.rollback()
     finally:
-        if db is not None:
-            db.close()
+        if session:
+            session.close()
 
 
 def get_avatar(domain, user_identifier=None, identifier_type='id'):
     avatar_url = None
     if not user_identifier:
         return avatar_url
-    query_map = {
-        'id': "select profile_picture from users where id = %s",
-        'username': "select profile_picture from users where username = %s"
-    }
 
-    if identifier_type not in query_map:
-        raise ValueError("identifier_type must be 'id' or 'username'")
+    session = None
     try:
-        with get_db_connection() as db:
-            with db.cursor() as cursor:
-                cursor.execute(query_map[identifier_type], (user_identifier,))
-                result = cursor.fetchone()
-                if result and result[0]:
-                    avatar_url = f"{domain}static/avatar/{result[0]}.webp"
-                    return avatar_url
-    except Exception as e:
-        pass
+        session = SessionLocal()
+        if identifier_type == 'id':
+            user = session.query(User).filter_by(id=user_identifier).first()
+        elif identifier_type == 'username':
+            user = session.query(User).filter_by(username=user_identifier).first()
+        else:
+            raise ValueError("identifier_type must be 'id' or 'username'")
+        if user and user.profile_picture:
+            avatar_url = f"{domain}static/avatar/{user.profile_picture}.webp"
+    except SQLAlchemyError as e:
+        print(f"Error fetching avatar: {e}")
+    finally:
+        if session:
+            session.close()
     return avatar_url
